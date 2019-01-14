82a6c98fa18ae03171cc4a1fb91e38da0fc54acd
DavidHamelin <hamelindavid@hotmail.fr>
2019-01-14 16:06:00 +0100

    js ex 04 part 02

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+﻿function resetForm()
+{
+    document.getElementById("lastname").value = " ";
+    document.getElementById("firstname").value = " ";
+    document.getElementById("city").value = " ";
+    return false;
+}
